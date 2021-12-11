d3cfcbadfb1e653b8514860b3f911f52a87a534e
matanatteyyubova <teyyubovametanet@gmail.com>
2021-12-11 17:19:39 +0400

    sidebar

---
+++ JavaScript/DOM/js/main.js
@@ -0,0 +1,86 @@
+
+
+
+
+
+
+
+
+
+ 
+function myFunc() {
+ var element = document.querySelector(".navbar");
+ element.classList.add("mynav");
+
+    
+
+}
+
+function myFunc1() {
+    var element = document.querySelector(".navbar");
+    element.classList.remove("mynav");
+       
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// DOM 
+// Documnet Object Model
+
+// let a = "Hell";
+// switch (a) {
+//     case (true||false):
+//         console.log("a is 5");
+//         break;
+        
+//     case "Hello":
+//         console.log('a is Hello');    
+//         break;
+
+//     default:
+//         console.log("this is false");
+//         break;
+// }
+
+// for(let a = 0; a<3; a++){
+//     document.getElementsByTagName('p')[a].innerHTML = "JavaScript"
+// }
+
+    // document.getElementsByTagName('p')[2].innerHTML = "JavaScript"
+    // document.getElementsByClassName('myLink')[0].innerHTML = "JavaScript"
+    // document.getElementsByClassName('myLink')[0].innerHTML = "JavaScript"
+    // document.getElementById('myColor').innerHTML = 'Green';
+    // document.querySelectorAll('.myLink')[0].innerHTML = 'Green';
+
+
+    // function myFunc(){
+    //     let a = 0;
+    //     while (a<3) {
+    //     document.querySelectorAll('p')[a].innerHTML = "JavaScript";
+    //        a++; 
+    //     }
+    // }
+   
+//     function myFunc(){
+       
+//         document.querySelector('a').attributes[1].value = "https://youtube.com"
+
+//     }
+//   myFunc()
+
+
+//     function myBlock(){
+//         document.querySelector('a').innerHTML = "Youtube";
+//         document.querySelector('a').attributes[1].value = "https://youtube.com"
+//         document.querySelector('a').attributes[0].value ="displayBlock green";
+//     }
+//     // myBlock()
